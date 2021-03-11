ffd28d96671ec92fece2b8217d904f7824185b8e
jmeza81 <77745592+jmeza81@users.noreply.github.com>
2021-03-10 21:38:23 -0600

    EX 7A - C# - Military Unit

---
+++ MilitaryUnit/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MilitaryUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateUnit();
+        }
+        public static void CreateUnit()
+        {
+            try
+            {
+                Personnel mSergeant = new Sergeant("Sergeant", "GI Joe");
+                mSergeant.Command = "Special Counter-Terrorism Unit Delta";
+                mSergeant.Shoot();
+
+                Console.WriteLine();
+
+                Vehicle myAttackVehicle = new VampMarkII("VampMarkII", 5);
+                myAttackVehicle.StartEngine(6);
+                myAttackVehicle.Accelerate();
+                myAttackVehicle.Brake();
+                myAttackVehicle.Mount();
+
+                Console.WriteLine();
+
+                Weapon myRifle = new M4("M4", ".556", 0);
+                myRifle.Fire(30);
+                myRifle.Reload();
+            }
+            catch( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                CreateUnit();
+            }
+            finally
+            {
+                Console.WriteLine("Yo Joe!");
+            }
+        }
+    }
+}
---
+++ MilitaryUnit/Vehicle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MilitaryUnit
+{
+    public abstract class Vehicle
+    {
+        public int Tires { get; set; }
+        public string Name { get; set; }
+        public Vehicle(string vModel, int wheels)
+        {
+            this.Name = vModel;
+            this.Tires = wheels;
+        }
+
+
+        public virtual void StartEngine(int wheels)
+        {
+            Console.WriteLine("Vroom");
+        }
+        
+        public virtual void Accelerate()
+        {
+            Console.WriteLine("The vehicle speeds up.");
+        }
+        
+        public virtual void Brake()
+        {
+            Console.WriteLine("The vehicle slows down and stops.");
+        }
+        
+        public virtual void Mount()
+        {
+            Console.WriteLine("The mount of the vehicle.");
+        }
+    }
+    public class VampMarkII : Vehicle
+    {
+        public VampMarkII(string vModel, int wheels) : base(vModel, wheels)
+        {
+            Console.WriteLine($"The {vModel} is a kick-ass all terrain vehicle. \n");
+        }
+
+        public override void StartEngine(int wheels)
+        {
+            Console.WriteLine("V v v v vroom.");
+            Console.WriteLine($"This vehicle has {wheels} tires");
+        }
+
+        public override void Accelerate()
+        {
+            Console.WriteLine("The Vamp Mark II is speeding up.");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine("The Vamp Mark II is slowing down to a stop.");
+        }
+
+        public override void Mount()
+        {
+            Console.WriteLine("The Vamp MArk II has a four barrel rocket launcher mounted on the bed.");
+        }
+    }
+}
